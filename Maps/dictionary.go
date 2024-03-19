6a53d0fe28471cca278bfb73fe84d6ffd8672f87
niralmaruda <85137760+niralmaruda@users.noreply.github.com>
2024-03-19 21:14:07 +0530

    dictionary

---
+++ Maps/dictionary.go
@@ -0,0 +1,15 @@
+package main
+
+import "errors"
+
+type Dictionary map[string]string
+
+var ErrorNotFound = errors.New("could not find the word you were looking for")
+
+func (d Dictionary) Search(word string) (string, error) {
+	defination, ok := d[word]
+	if !ok {
+		return "", ErrorNotFound
+	}
+	return defination, nil
+}
